999fa76b50741927add07580086b0371b8a31040
Anna Trunova <trunova.am@yandex.ru>
2021-12-27 19:48:00 +0500

    change color

--- pss.py
+++ pss.py
@@ -12,24 +12,28 @@ class Board:
         self.cell_size = 30
         self.spy = []
         self.spx = []
+        self.cou = 0
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
+        self.dl = self.height * self.cell_size
+        self.sh = self.width * self.cell_size
 
     def get_cell(self, pos):
         x, y = pos
         a = ((x - self.left) // self.cell_size) + 1
         b = (((y - self.top) // self.cell_size) + 1)
-        if a < 1 or a > self.height or b < 1 or b > self.height:
+        if a < 1 or a > self.width or b < 1 or b > self.height:
             return None
         else:
-            return (a, b)
+            return a, b
 
     def on_click(self, cell):
         if cell:
+            self.draw(screen, cell)
             print(cell)
         else:
             print('None')
@@ -51,17 +55,31 @@ class Board:
                 self.spx.append(x)
                 self.spy.append(y)
 
+    def draw(self, screen, cell):
+        x, y = cell
+        if self.board[x - 1][y - 1] == 1:
+            self.board[x - 1][y - 1] = 0
+        else:
+            self.board[x - 1][y - 1] = 1
+        pygame.draw.rect(screen, colors[self.board[x - 1][y - 1]], (
+            ((x - 1) * self.cell_size) + self.left,
+            ((y - 1) * self.cell_size) + self.top,
+            self.cell_size,
+            self.cell_size))
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Инициализация игры')
-    size = width, height = 600, 600
+    size = width, height = 700, 700
     screen = pygame.display.set_mode(size)
 
-    colors = ['black', 'white', 'pink']
+    colors = ['white', 'black']
 
-    board = Board(5, 5)
-    board.set_view(50, 50, 100)
+    board = Board(15, 15)
+    board.set_view(50, 50, 30)
+    screen.fill((255, 255, 255))
 
     running = True
     while running:
@@ -70,8 +88,8 @@ if __name__ == '__main__':
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
-        screen.fill((0, 0, 0))
-        board.render(screen)
+                pygame.display.flip()
         pygame.display.flip()
+        board.render(screen)
 
     pygame.quit()
